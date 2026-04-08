de12398aa7c646465755f45a2194c159402441aa
michelkazi <mkazi@strava.com>
2026-04-08 13:43:46 -0600

    v0.0.1

--- Formula/cartographer.rb
+++ Formula/cartographer.rb
@@ -1,13 +1,13 @@
 class Cartographer < Formula
   desc "Keyboard-driven grid window tiler for macOS (like Tactile for GNOME)"
   homepage "https://github.com/MichelKazi/cartographer"
-  version "0.1.0"
+  version "0.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/MichelKazi/cartographer/releases/download/v0.1.0/cartographer"
-      sha256 "72e50707acda4c21d29c33a717c9e80c1f8faa2ec66acc082c9fa13303c434a3"
+      url "https://github.com/MichelKazi/cartographer/releases/download/v0.0.1/cartographer"
+      sha256 "fe790d61ca3e3e19ddfb001344f906e98e3e3c162631ea2496f9e10ddcb8aa41"
     end
   end
 
